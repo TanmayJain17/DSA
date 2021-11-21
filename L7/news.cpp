a9f0c7fa17274fe2ef54a51373a174fa47752d02
Tanmay <tanmay.jain260@gmail.com>
2021-11-21 12:52:44 +0530

    codes

---
+++ L7/news.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int x = 0, y = 0;
+	char ch;
+	cin >> ch;
+	while (ch != '\n') {
+		if (ch == 'E') {
+			x++;
+		}
+		else if (ch == 'W')
+			x--;
+		else if (ch == 'N')
+			y++;
+		else
+			y--;
+
+		ch = cin.get();
+	}
+
+	if (x > 0) {
+		for (int i = 1; i <= x; i++) {
+			cout << 'E';
+		}
+	}
+	if (y > 0) {
+		for (int i = 1; i <= y; i++) {
+			cout << 'N';
+		}
+	}
+	//can also use abs(y) (//absolute value)
+	if (y < 0) {
+		for (int i = -1; i >= y; i--) {
+			cout << 'S';
+		}
+	}
+	if (x < 0) {
+		for (int i = -1; i >= x; i--) {
+			cout << 'W';
+		}
+
+	}
+	return 0;
+
+}
