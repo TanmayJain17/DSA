065eae7ef7b5fd3546aad3e15dd9958327c1e7b9
Tanmay <tanmay.jain260@gmail.com>
2021-12-12 12:27:49 +0530

    directory-Changed-recursion

---
+++ L13/printNumbers.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printNumbers(int arr[],int n,int i){
+	if()
+}
+
+int main(){
+	int n;
+	cin>>n;
+	int arr[100000];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	cout<<printNumbers(arr,n,0)
+	return 0;
+}
